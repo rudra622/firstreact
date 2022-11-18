ac19fcfb001c9efd20e83ec5432a2aebb3d5030e
rudra622 <rudradobariya2862004@gmail.com>
2022-11-18 15:20:57 +0530

    Hello

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <h2>RUdra Dobariya</h2>
+      <p>Web Devlopment</p>
+    </div>
+  );
+}
+
+export default App;
